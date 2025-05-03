c744b04591d3f1210f3d24dfd5141180c2f10284
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-05-03 16:32:12 -0300

    Adicionada a propriedade dataHora, para indicar a data e hora da doação

--- src/app/models/doacao.model.ts
+++ src/app/models/doacao.model.ts
@@ -3,11 +3,13 @@ export class Doacao {
     valor: number;
     idDoador: number;
     idCampanha: number;
+    dataHora: Date;
 
-    constructor(id: number, valor: number, idDoador: number, idCampanha: number){
+    constructor(id: number, valor: number, idDoador: number, idCampanha: number, dataHora: Date){
         this.id = id;
         this.valor = valor;
         this.idDoador = idDoador;
         this.idCampanha = idCampanha;
+        this.dataHora = dataHora;
     }
 }
