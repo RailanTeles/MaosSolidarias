b8a3e8eaa1d9c5674ac9801de9b5b62ef137fcc6
beaxtryz <anasilvaprados@gmail.com>
2025-06-08 01:43:25 -0300

    [bia]

--- src/app/Components/perfil/perfil.component.ts
+++ src/app/Components/perfil/perfil.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
